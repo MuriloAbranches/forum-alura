5c48fc61f6ffebc0e574fce63deff5e71f864ec7
Murilo Abranches <muriloads@hotmail.com>
2021-02-25 23:10:04 -0300

    Adicionando Cache e Autenticação

---
+++ src/main/java/com/muriloabranches/forum/config/security/TokenService.java
@@ -0,0 +1,38 @@
+package com.muriloabranches.forum.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import com.muriloabranches.forum.modelo.Usuario;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+	
+	@Value("${forum.jwt.secret}")
+	private String secret;
+	
+	@Value("${forum.jwt.expiration}")
+	private Long expiration;
+
+	public String gerarToken(Authentication authentication) {
+		
+		Usuario logado = (Usuario) authentication.getPrincipal();
+		Date hoje = new Date();
+		Date dataExpiracao = new Date(hoje.getTime() + expiration);
+		
+		return Jwts.builder()
+				.setIssuer("API do Forúm da Alura")
+				.setSubject(logado.getId().toString())
+				.setIssuedAt(hoje)
+				.setExpiration(dataExpiracao)
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.compact();
+	}
+
+}
